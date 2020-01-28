8dd270237142c44c6b6d067991802f0a1bbe030a
eridanletalis <31878137+eridanletalis@users.noreply.github.com>
2020-01-28 09:11:34 +0300

    Add files via upload
    Дз третьей недели обучения
    
    ХЗ, что там с функцией стоимости после регуляризации, но на тестовом примере срабатывает.

---
+++ ex2/sigmoid.m
@@ -0,0 +1,24 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid function
+%   g = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+asize = size(g);
+
+if (asize(1) == 1) && (asize(2) == 1)
+	g = 1 / (1 + exp(-z));
+end
+
+if asize(1) > 1 || asize(2) > 1
+	g = 1 ./ (exp(-z) + 1)
+end
+
+% =============================================================
+
+end
